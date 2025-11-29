ceb6bfcc3551037e362c8b6c2b40e69190a0901a
nhhung18 <nguyenhung18042005@gmail.com>
2025-11-29 10:46:54 +0700

    create base backend

---
+++ backend/hab_lib/src/main/java/com/g3/hab_lib/HabLibApplication.java
@@ -0,0 +1,13 @@
+package com.g3.hab_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabLibApplication.class, args);
+	}
+
+}
---
+++ backend/hab_lib/src/test/java/com/g3/hab_lib/HabLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3.hab_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
